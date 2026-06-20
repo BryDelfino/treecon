9763a238176d72aba7b115fd2250fa223d2d1509
BryDelfino <bvdelfino@up.edu.ph>
2026-06-20 22:27:31 +0800

    added sign up page in the routes.

--- apps/scout_mobile/lib/src/app.dart
+++ apps/scout_mobile/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scout_mobile/src/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:scout_mobile/src/features/auth/presentation/pages/sign_up_page.dart';
 import 'package:scout_mobile/src/features/home/presentation/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const SignInPage(),
+        '/signup': (context) => const SignUpPage(),
         '/home': (context) => const HomePage(),
       },
     );
