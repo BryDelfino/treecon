f93a75d4f925adb192bb0f6362c6ec1646407da4
BryDelfino <bvdelfino@up.edu.ph>
2026-06-20 12:42:22 +0800

    created app.dart containing all pages and routes.

---
+++ apps/scout_mobile/lib/src/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:scout_mobile/src/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:scout_mobile/src/features/home/presentation/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Treecon Scout',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SignInPage(),
+        '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
