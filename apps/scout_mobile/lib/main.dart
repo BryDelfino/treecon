c69b426714339d24bd67557dc92611639176ca03
BryDelfino <bvdelfino@up.edu.ph>
2026-06-20 12:41:51 +0800

    converted main.dart as entry point to app.dart.

--- apps/scout_mobile/lib/main.dart
+++ apps/scout_mobile/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_services/shared_services.dart';
+import 'package:scout_mobile/src/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:scout_mobile/src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,58 +12,4 @@ void main() async {
   );
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Todos',
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final Stream<List<Todo>> _stream;
-
-  @override
-  void initState() {
-    super.initState();
-    final todoService = TodoService(Supabase.instance.client);
-    _stream = todoService.getTodosStream();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<List<Todo>>(
-        stream: _stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          final todos = snapshot.data!;
-          return ListView.builder(
-            itemCount: todos.length,
-            itemBuilder: ((context, index) {
-              final todo = todos[index];
-              return ListTile(
-                title: Text(todo.name),
-              );
-            }),
-          );
-        },
-      ),
-    );
-  }
 }
