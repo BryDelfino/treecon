a795bbad96caa1f0a0ab96372d9ebdd63106c6b3
BryDelfino <bvdelfino@up.edu.ph>
2026-07-01 23:40:25 +0800

    added instance of network service and local db for offline mode.

--- apps/scout_mobile/lib/main.dart
+++ apps/scout_mobile/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_services/shared_services.dart';
 import 'package:scout_mobile/src/app.dart';
+import 'package:scout_mobile/src/core/services/network_service.dart';
+import 'package:scout_mobile/src/features/observations/data/observation_local_db.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,6 +11,10 @@ void main() async {
     url: const String.fromEnvironment('SUPABASE_URL'),
     publishableKey: const String.fromEnvironment('SUPABASE_PUBLISHABLE_KEY'),
   );
+
+  // Initialize NetworkService & local database wrapper
+  await NetworkService.instance.init();
+  await ObservationLocalDb.instance.open();
 
   runApp(const MyApp());
 }
