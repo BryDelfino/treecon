9c0181c0b7443b3c973b4b3f3d02492fe45096ab
BryDelfino <bvdelfino@up.edu.ph>
2026-07-15 22:28:14 +0800

    deleted core.dart contents.

--- packages/core/example/core_example.dart
+++ packages/core/example/core_example.dart
@@ -1,7 +1,3 @@
-import 'package:core/core.dart';
-import 'dart:developer' as developer;
-
 void main() {
-  var awesome = Awesome();
-  developer.log('awesome: ${awesome.isAwesome}', name: 'my.app.category');
+  // Example usage of core package
 }
