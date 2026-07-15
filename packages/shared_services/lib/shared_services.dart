bb290a8ba8cb70a75df69ec5eb9cdf095066d8af
BryDelfino <bvdelfino@up.edu.ph>
2026-07-15 22:28:57 +0800

    exported province lookup service.

--- packages/shared_services/lib/shared_services.dart
+++ packages/shared_services/lib/shared_services.dart
@@ -1,4 +1,5 @@
 export 'package:supabase_flutter/supabase_flutter.dart';
 export 'src/todo_service.dart';
 export 'src/user_service.dart';
+export 'src/province_lookup.dart';
 export 'package:shared_models/shared_models.dart';
